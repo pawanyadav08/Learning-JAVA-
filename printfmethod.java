96dccefaba245630e2b7f4f626aac2b236d57713
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-03-27 11:29:06 +0530

    Update printf method example

--- printfmethod.java
+++ printfmethod.java
@@ -1,5 +1,17 @@
 public class printfmethod {
     public static void main(String[] args) {
-        
+      boolean myBoolean = true;
+      char myChar = '@';
+      int myInt = 50;
+      String myString = "Bro";
+      double myDouble = 1000;
+      // System.out.printf("%b",myBoolean);
+      // System.out.printf("%c",myChar);
+      // System.out.printf("%s",myString);
+      // System.out.printf("%d",myInt);
+      // System.out.printf("%f",myDouble);
+      // System.out.printf("Hello %-10s",myString);
+      // System.out.printf("You have this much money %.2f",myDouble);
+      System.out.printf("You have this much money %,f",myDouble);
     }
 }
