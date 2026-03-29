730796a8167b3a7e188c374a9d822accd57294df
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-03-29 13:05:22 +0530

    Add human class example

---
+++ human2.java
@@ -0,0 +1,11 @@
+// constructor
+public class human2 {
+    public static void main(String[] args) {
+      Human human1 = new Human("Rick",65,70);
+      Human human2 = new Human("Morty",16,50);  
+
+      System.out.println(human2.name);
+      human2.eat();
+      human1.drink();
+    }
+}
