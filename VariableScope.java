0437fcebe71d0f5e31c9400b1ab5e36f0ba1c225
Pawan Kumar Yadav <pawanydv010@gmail.com>
2026-04-03 20:21:48 +0530

    Fix VariableScope dice roller initialization

--- VariableScope.java
+++ VariableScope.java
@@ -1,6 +1,21 @@
+import java.util.Random;
+
+class DiceRoller{
+
+    int number;
+    Random random;
+    DiceRoller(){
+        this.random = new Random();
+        roll();
+    }
+
+    void roll(){
+        number = random.nextInt(6)+1;
+        System.out.println(number);
+    }
+}
 public class VariableScope {
     public static void main(String[] args) {
-        
-        DiceRoller diceRoller = new DiceRoller();
+      DiceRoller diceRoller = new DiceRoller();  
     }
 }
--- variable_scope.java
+++
@@ -1,6 +0,0 @@
-public class variable_scope {
-    public static void main(String[] args) {
-        // Delegate to the correctly named class so both entry points work
-        VariableScope.main(args);
-    }
-}
